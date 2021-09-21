a0d21c1af3b644e7c9aca92d03721081e8461ee8
yosephine17 <60084849+yosephine17@users.noreply.github.com>
2021-09-21 20:18:08 +0700

    Update SE-asdos.py

--- SE-asdos.py
+++ SE-asdos.py
@@ -1,3 +1,58 @@
-#variable
+#VARIABLE
 
-#integer
+#INTEGER
+a = 5
+b = 6
+c = a+b
+
+#STRING
+kampus = "President University"
+
+#BOOLEAN
+D = True
+E = False
+
+#FLOAT
+F = 8.1
+
+#LIST/ARRAY
+G = []
+
+#DICTIONARY
+H = {}
+
+#TUPLE
+I = ()
+
+#PRINT
+print(kampus)
+print("Yosephine")
+print(5)
+print(1+2)
+print(True)
+
+#PRINT02
+print("Saya kuliah di : " + kampus)
+print("Adik saya ada: " + str(a))
+print("Adik saya ada: ".format(b))
+print("Nama saya: {}, Asal: {}, Umur: {}".format("Yosephine", "Banda Aceh", 21))
+
+#LIST/ARRAY
+Bunga = ["Mawar", "Melati", "Tulip", "Bougenville"]
+
+#PRINT LIST
+print(Bunga[0])
+print(Bunga[1])
+print(Bunga[2])
+print(Bunga[3])
+
+ulang = ["3 kali"] * 3
+ulang
+
+print(Bunga[0:2]) #print list 0 dan 1
+Bunga[-2] #print list urutan ke 2 dari belakang
+Bunga[-2:] #print 2 list terakhir
+
+#APPEND
+Bunga.append("Lily")
+print(Bunga)
